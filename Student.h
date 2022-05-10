743f2f7324d34bff401d8cb9c8010e13444b7e8b
Ilya <ilya.trishkin.03@mail.ru>
2022-05-10 19:32:32 +0300

    Добавьте файлы проекта.

---
+++ Student.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+#include<string>
+
+
+class Student
+{
+public:
+	Student();
+	~Student();
+
+	friend std::ostream& operator <<(std::ostream& os, const Student& b);
+	friend std::istream& operator >>(std::istream& is, Student& b);
+
+private:
+	std::string name;
+	std::string gender;
+	std::string date_of_birth;
+	int group;
+	double average_ball;
+};
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "Student.h"
+
+int main()
+{
+	Student a;
+	std::cout << a;
+
+	std::cin >>a;
+	std::cout << a;
+}
